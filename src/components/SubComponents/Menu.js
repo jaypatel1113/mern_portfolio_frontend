c24a313ec49f2230a99e2f6c8c096dfb8e7fec39
Jay Patel <jay734883@gmail.com>
2023-02-17 12:56:32 +0530

    added backto top on click... in menulink

--- src/components/SubComponents/Menu.js
+++ src/components/SubComponents/Menu.js
@@ -2,8 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Menu = ({ i, clr, setClick, click, href, title, svg }) => {
+    const backToTop = () => {
+        setClick(!click);
+        window.scrollTo({ top: 0 });
+    };
+
     return (
-        <li style={{ "--i": i, "--clr": clr }} onClick={() => setClick(!click)}>
+        <li
+            style={{ "--i": i, "--clr": clr }}
+            onClick={() => backToTop()}
+        >
             <NavLink to={href} className="navbar-link" data-title={title}>
                 {svg}
             </NavLink>
