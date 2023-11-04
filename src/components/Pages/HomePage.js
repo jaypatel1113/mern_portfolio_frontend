de129141237e08eb2d19b2fee232caeadf4292b8
Jay Patel <jay64441860@gmail.com>
2023-11-04 22:28:19 +0530

    added redirect to main porfolio site

--- src/components/Pages/HomePage.js
+++ src/components/Pages/HomePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Banner from "../Banner/Banner";
@@ -7,6 +7,7 @@ import Projects from "../Projects/Projects";
 import Contact from "../Contact/Contact";
 import AboutMe from "../AboutMe/AboutMe";
 import EducationWork from "../EducationWork/EducationWork";
+import { useLocation } from "react-router-dom";
 
 const HomePage = () => {
     const { user } = useSelector((state) => state.user);
@@ -14,6 +15,17 @@ const HomePage = () => {
     // useEffect(() => {
     //     console.log(user);
     // }, [user]);
+
+    // redirect user to main porfolio site
+    const location = useLocation();
+    useEffect(() => {
+        const queryParams = new URLSearchParams(location.search);
+        const redirectParam = queryParams.get('redirect');
+
+        if (redirectParam !== 'false') {
+            window.location.href = 'https://patel-jay.vercel.app/';
+        }
+    }, [])
 
     return (
         <>
